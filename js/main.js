2cee9e6fd33ab36136cb26a74ffed76a747b31fb
Virginie <vbouvarel@lilo.org>
2020-05-07 19:22:05 +0200

    validation avant suivant/resultats WIP

--- js/main.js
+++ js/main.js
@@ -18,26 +18,49 @@
     console.log(scores);
 
 /*
-2/ Au clic sur la case de validation des réponses, on calcule les scores pour la page en cours et on met à jour l'objet scores en ajoutant les valeurs trouvées aux valeurs déjà sauvegardées pour chaque propriété
+2/ Au clic sur la case de confirmation des réponses, on calcule les scores pour la page en cours et on met à jour l'objet scores en ajoutant les valeurs trouvées aux valeurs déjà sauvegardées pour chaque propriété. 
+En fonction de l'état cochée/décochée de la case on active/désactive le bouton pour passer au questionnaire suivant.
 */
-    if(document.querySelector(".confirmation")){
-        document.querySelector(".confirmation").addEventListener("click", setScores);
-    }
+    let confirmation = document.querySelector(".confirmation");
+    let buttonNext = document.querySelector("#next");
+    
+    if(confirmation){
+        confirmation.addEventListener("click", setScores);
+        confirmation.addEventListener("change", function(){
+                if(buttonNext){
+                    buttonNext.classList.toggle("disabled");
+                }
+            });
+        }
 
 
 /*
-3/ Au clic sur le bouton submit:
+3/ Au clic sur case de validation du questionnaire:
 - on détermine les lettres du profil en fonction des scores 
 - on passe leur valeurs au html
 - on reset le localStorage
+En fonction de l'état cochée/décochée de la case on active/désactive le bouton submit.
 */
-    if(document.querySelector(".validation")){
-        document.querySelector(".validation").addEventListener("click", setProfile);
+    let validation = document.querySelector(".validation");
+    let buttonResults = document.querySelector("#results");
+    if(validation){
+        validation.addEventListener("click", setProfile);
+        validation.addEventListener("change", function(){
+            buttonResults.classList.toggle("disabled");
+        })
     }
     
+// /*
+// 4/ Au clic sur les boutons next ou results désactivés, on affiche un message pour indiquer que les cases de confirmation/validation ne sont pas cochées
+// */
+//     let disabled = document.querySelector(".disabled");
+//     console.log(disabled);
+//     disabled.addEventListener("mouseover", function(){
+//         console.log("ajoute un div message d'erreur");
+//     });
 
 /*
-4/ Au clic sur les boutons de profils dans la page resultsDouble.phtml on affiche un profil ou l'autre
+5/ Au clic sur les boutons de profils dans la page resultsDouble.phtml on affiche un profil ou l'autre
 */
     toggleProfile();
     
