f3cbd882fe4d5ee0a66d5d7fa260a7bbab101614
Virginie <vbouvarel@lilo.org>
2020-06-17 12:36:15 +0200

    final commit

--- bdd.php
+++ bdd.php
@@ -1,11 +1,8 @@
 <?php
 
-
 /*CODE POUR SE CONNECTER A LA BDD*/
 //local
 $pdo = new PDO('mysql:host=localhost;dbname=riasec;charset=UTF8', "root", "", [ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-
 
 
 
