96dab6454f66eedf7dbbaaff2f4d7b4de499d88b
Virginie <vbouvarel@lilo.org>
2020-08-08 09:43:27 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function helloIronhack () {
+    return "Hello Ironhacker!";
+  }
+
+//exo My calculator
+
+function sumArray(array){
+    return (array.length < 1 ? 0 : array.reduce((sum,value) => sum + value));
+}
