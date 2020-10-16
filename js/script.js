8321daae4050b62114b49a192b89835e93c5d3c5
Virginie <vbouvarel@lilo.org>
2020-10-16 12:49:32 +0200

    commit avant surtension

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const CANVAS_HEIGHT = 600;
 const ARROW_RIGHT = 39;
 const ARROW_LEFT = 37;
 
-
 let canvas;
 let ctx;
 let paddle; 
 let ball;
 let interval;
 let delay = 15;//ms
+
 
 class Paddle {
     constructor(posX, posY, width, height, color) {
@@ -47,16 +47,16 @@ class Paddle {
 }
 
 class Ball {
-    constructor (x, y, radius, color, direction) {
+    constructor (x, y, radius, color) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
-        this.direction = direction;
-        
-
+        this.nextPosX = this.x - 2;
+        this.nextPosY = this.y + 2;
     }
 
+   
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
@@ -65,41 +65,48 @@ class Ball {
         ctx.closePath();
     }
 
-    checkCollision(nextPosX, nextPosY) {
-
-        //On détermine les coordonnées des bords du canvas
+    checkCollision() {
+        //On détermine les coordonnées des bords du canvas en fonction du rayon de la balle
         const leftEdge = 0 + ball.radius;//x = 10;
         const rightEdge = CANVAS_WIDTH - ball.radius; //x= 890;
         const topEdge = 0 + ball.radius;//y = 10;
         const bottomEdge = CANVAS_HEIGHT - ball.radius; //y = 590;
 
-        //On compare les coordonnées du bord de la balle avec ceux des bords du canvas
-        if (nextPosX <= leftEdge) return "left";
-        if (nextPosX >= rightEdge) return "right";
-        if (nextPosY <= topEdge) return "top";
-        if (nextPosY >= bottomEdge) return "bottom";
+        if (this.nextPosX <= leftEdge) return "left";
+        if (this.nextPosX >= rightEdge) return "right";
+        if (this.nextPosY <= topEdge) return "top";
+        if (this.nextPosY >= bottomEdge) return "bottom";
     }
 
     setNextPosition() {
-        const nextPosX = this.x - this.direction;
-        const nextPosY = this.y + this.direction;
 
-        const collision = this.checkCollision(nextPosX, nextPosY);
+        //TODO: trouver le moyen d'incrémenter nextPosX et nextPosY  de 2 à chaque tour, en respectant le signe: si x va dans le sens x+2 alors nextposx = x + 2 mais si x va dans le sens x-2 alors nextPosX = x - 2. incrémenter = ajouter, il faut toujours ajouter 2 mais en + ou 2 en - selon le cas ---> + 2 ou + (-2) = + 2 ou - 2 en utilisant toujours l'incrémentation. du coup nextPosX = x + ?, ? étant soit +2 soit - 2
+        const collision = this.checkCollision();
 
-        if (collision === "left" || collision === "right") {
-            this.direction = - this.direction;
-           
+        if (collision === "left") {
+            console.log("collision left");
+            this.nextPosX = this.x + 2;
         }
-        if (collision === "top" || collision === "bottom") {
-            this.direction = - this.direction;
+        if (collision === "right") {
+            console.log("collision right");
+            this.nextPosX = this.x - 2;
+        }
+        if (collision === "top") {
+            console.log("collision top");
+            this.nextPosY = this.y + 2;
+        }
+        if (collision === "bottom") {
+            console.log("collision bottom");
+            this.nextPosY = this.y - 2;
         }
 
-        this.x = nextPosX;
-        this.y = nextPosY;
-        
+        this.x = this.nextPosX;
+        this.y = this.nextPosY;
+   
     }
 
     move() {
+        
         this.setNextPosition();
         this.draw();
     }
@@ -124,7 +131,7 @@ function init() {
 
     paddle = new Paddle(375, 570, 150, 30, "#fff");
     paddle.draw();
-    ball = new Ball(450, 11, 10, "#fff", 2);
+    ball = new Ball(450, 10, 10, "#fff");
     ball.draw();
 
     window.addEventListener("keydown", handleKeyDown);
