b611c45107d9774691fb26f4e9f3eaa93e86aafc
Virginie <vbouvarel@lilo.org>
2020-12-07 13:13:59 +0100

    + logique initiale detection collision ball-brick

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ class Game {
         this.startMsg = document.querySelector('.start-msg');
         this.scoreTag = document.querySelector('#score');
         this.score = 0;
-        this.bricksNumber = 21;
         this.stopped = true;
         this.animationID;
 
         this.court = new Canvas(900, 600, 50);
         this.paddle = new Paddle(375, 570, 150, 30, "#fff", this.court.ctx, this.court.borderWidth);
-        this.ball = new Ball(450, 560, 10, "#fff", 5, this.court.ctx, this.paddle);
+        
+        this.ball = new Ball(450, 560, 10, "#fff", 5, this.court.ctx, this.paddle, this.bricks);
  
         
 
@@ -21,7 +21,7 @@ class Game {
     }
 
     init() {
-        this.bricks = this.buildBricksArray(); 
+        this.bricks = this.buildBricksArray();    
         this.displayScore();
         this.displayBricks();  
         this.paddle.draw();
@@ -63,12 +63,13 @@ class Game {
 
     refresh() {
         if (!this.stopped) {
-            let collision = this.ball.move();
+            let collision = this.ball.move(this.bricks);
     
             if (collision.bottom) {
                 this.gameOver();
             } else {
-                if (collision.paddle) {
+                if (collision.brick.index > -1) {
+                    this.deleteBrick(collision.brick.index);
                     this.updateScore();
                     this.updateSpeed();
                 }
@@ -139,6 +140,10 @@ class Game {
         for (let i = 0; i < this.bricks.length; i++) {
             this.bricks[i].draw();
         }
+    }
+
+    deleteBrick(index) {
+       this.bricks.splice(index, 1);
     }
 }
   
@@ -242,7 +247,7 @@ class Ball {
         this.paddle = paddle;
 
 
-        this.directionX = this.speedInPixel;
+        this.directionX = - this.speedInPixel;
         this.directionY = - this.speedInPixel;
         this.leftEdge = this.radius;
         this.rightEdge = this.ctx.canvas.width - this.radius;
@@ -263,17 +268,17 @@ class Ball {
         this.ctx.closePath();
     }
 
-    resetCoordinates() {
+    resetCoordinates(bricks) {
         let newCoordinates = this.calculateNextPosition();
 
-        let collision = this.detectCollision(newCoordinates);
+        let collision = this.detectCollision(newCoordinates, bricks);
 
         if (!collision.bottom) { 
             // Si la balle dépasse le canvas on inverse le sens pour générer l'effet de rebond
-            if (collision.left || collision.right) {
+            if (collision.left || collision.right ||collision.brick.left ||collision.brick.right) {
                 this.directionX = - this.directionX;
             }
-            if (collision.top || collision.paddle){
+            if (collision.top || collision.paddle || collision.brick.top ||collision.brick.bottom){
                 this.directionY = - this.directionY;
             }
             
@@ -287,19 +292,54 @@ class Ball {
         return {x: this.x + this.directionX, y: this.y + this.directionY};
     }
 
-    detectCollision(newCoordinates) {
+    detectCollision(newCoordinates, bricks) {
         return {
             left: newCoordinates.x <= this.leftEdge,
             right: newCoordinates.x >= this.rightEdge,
             top: newCoordinates.y <= this.topEdge,
             bottom: newCoordinates.y >= this.bottomEdge,
-            paddle: newCoordinates.x >= this.paddle.x && newCoordinates.x <= this.paddle.x + this.paddle.width && newCoordinates.y >= this.bottomEdge - this.paddle.height
+            paddle: newCoordinates.x >= this.paddle.x && newCoordinates.x <= this.paddle.x + this.paddle.width && newCoordinates.y >= this.bottomEdge - this.paddle.height,
+            brick: this.detectBrickCollision(newCoordinates, bricks)
         }
     }
 
-    move() {
+    detectBrickCollision(newCoordinates, bricks) {
+          //on initialise un objet impact: {brick: index = -1, top: false, right: false, bottom: false, left: true, impact:true}, les valeurs par défaut indiquent une absence d'impact
+        const impact = {
+            index: -1, //en-dehors de bricks
+            top:false,
+            right:false,
+            bottom:false,
+            left:false
+        }
+        //Parcours le tableau de briques
+        for (let i = 0; i < bricks.length; i++) {
+             //Pour chaque brique on verifie si elle remplie les conditions d'une collision avec un des bords, si oui on modifie impact 
+            if (newCoordinates.y + this.radius === bricks[i].y && newCoordinates.x >= bricks[i].x && newCoordinates.x <= bricks[i].x + bricks[i].width) {
+                impact.top = true;
+            }
+            if (newCoordinates.y - this.radius === bricks[i].y + bricks[i].height && newCoordinates.x >= bricks[i].x && newCoordinates.x <= bricks[i].x + bricks[i].width) {
+                impact.bottom = true;
+            }
+            if (newCoordinates.x - this.radius === bricks[i].x + bricks[i].width & newCoordinates.y >= bricks[i].y && newCoordinates.y <= bricks[i].y + bricks[i].height) {
+                impact.right = true;
+            }
+            if (newCoordinates.x + this.radius === bricks[i].x && newCoordinates.y >= bricks[i].y && newCoordinates.y <= bricks[i].y + bricks[i].height) {
+                impact.left = true;
+            }
+            if(impact.top || impact.right || impact.bottom || impact.left) {
+                impact.index = i;
+                break; //une seule collision possible à chaque mouvement de ball
+            }
+        }
+
+        //Dans tous les cas on retourne l'objet
+        return impact;
+    }
+
+    move(bricks) {
         this.clear();
-        let collision = this.resetCoordinates();
+        let collision = this.resetCoordinates(bricks);
         this.draw();
         return collision;
     }
@@ -327,6 +367,8 @@ class Brick {
         this.height = height;
         this.color = color;
         this.ctx = ctx;
+        this.edges = []
+
 
     }
 
