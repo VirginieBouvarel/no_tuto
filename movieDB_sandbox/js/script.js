0630ed4f501f289ab3dbd4ef74ea3bc3ace77ee6
Virginie <vbouvarel@lilo.org>
2021-02-09 13:13:49 +0100

    getUserInput() vers console

--- movieDB_sandbox/js/script.js
+++ movieDB_sandbox/js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const input = document.querySelector("#search_input");
+    const btn = document.querySelector("#search_btn");
+
+    
+    btn.addEventListener('click', getUserInput);
+    
+
+    function getUserInput(event) {
+        event.preventDefault();
+        console.log(input.value);
+    }
+    
+    
+});
+
